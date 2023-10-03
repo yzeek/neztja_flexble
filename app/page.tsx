94cef962f3f6caf25b53ff9b0d35e0241b915a5c
yzeek <isaac.cohen1978@gmail.com>
2023-10-04 00:48:28 +0300

    components, constants

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-const Home = () => {
+const Home = async () => {
+  //   if (projectsToDisplay.length === 0) {
+  //     return (
+  //       <section className="flexStart flex-col paddings">
+  //         <p className="no-result-text text-center">
+  //           No projects found, go create some first.
+  //         </p>
+  //       </section>
+  //     );
+  //   }
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="flexStart flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load more</h1>
+    </section>
   );
 };
+
 export default Home;
