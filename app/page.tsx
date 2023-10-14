418e66cedb7c148589fcf93e3a34626212879823
yzeek <isaac.cohen1978@gmail.com>
2023-10-14 12:42:18 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+const Home = async () => {
+  //   if (projectsToDisplay.length === 0) {
+  //     return (
+  //       <section className="flexStart flex-col paddings">
+  //         <p className="no-result-text text-center">
+  //           No projects found, go create some first.
+  //         </p>
+  //       </section>
+  //     );
+  //   }
+
+  return (
+    <section className="flexStart flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load more</h1>
+    </section>
+  );
+};
+
+export default Home;
